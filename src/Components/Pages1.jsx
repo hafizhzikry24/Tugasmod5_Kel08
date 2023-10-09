77d68aaf0aa68a005875ce24f27c54b412ed4f4b
Faizal Lutfi Yoga Triadi <42670670+ABCDullahh@users.noreply.github.com>
2023-10-09 23:55:34 +0700

    1

--- src/Components/Pages1.jsx
+++ src/Components/Pages1.jsx
@@ -1,33 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from 'react-router-dom'; 
+import { useContext } from "react";
+import { PokemonContext } from "./PokemonDatas";
+import { Link } from 'react-router-dom';
 
 function PokemonList() {
-  const [pokemonList, setPokemonList] = useState([]);
-
-  useEffect(() => {
-    async function getPokemonList() {
-      try {
-        const res = await axios.get("https://pokeapi.co/api/v2/pokemon/");
-        const responseData = res.data.results; 
-        setPokemonList(responseData);
-      } catch (error) {
-        console.log(error);
-        window.alert(error);
-      }
-    }
-    getPokemonList();
-  }, []);
+  const { pokemonList } = useContext(PokemonContext);
 
   return (
     <div>
       <div>
         <h2 >Pokemon Halaman 1</h2>
-    
-          {pokemonList.slice(0, 10).map((pokemon, index) => (
-            <li key={index}>{pokemon.name}</li>
-          ))}
-    
+
+        {pokemonList.slice(0, 10).map((pokemon, index) => (
+          <li key={index}>{pokemon.name}</li>
+        ))}
+
       </div>
 
       <Link to="/halaman2">
